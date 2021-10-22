0791dbd5d4370765484fd8d5c0aa92f4444961e9
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-10-22 09:36:32 +0330

    add Calculator.java

---
+++ Part2/src/main/java/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import static java.lang.Math.sqrt;
+
+public class Calculator {
+    public double sqr(int a){
+        // Todo
+        return sqrt(a);
+    }
+
+    public double rvs(int a){
+        //Todo
+        return 1.0 / a;
+    }
+
+    public double operate(int a, String opt){
+        if(opt.equals("rvs"))
+            return rvs(a);
+        else if(opt.equals("sqr"))
+            return sqr(a);
+        throw new RuntimeException("Operator not defined.");
+    }
+}
